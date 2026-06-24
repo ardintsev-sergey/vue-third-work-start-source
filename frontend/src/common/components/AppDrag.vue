de9b5fd22fe12ce60f0dc018d2cf83e1ed047588
Сергей Ардинцев <ardicev@diorit.ru>
2026-06-24 14:03:11 +0300

    http api add

--- frontend/src/common/components/AppDrag.vue
+++ frontend/src/common/components/AppDrag.vue
@@ -1,14 +1,19 @@
 <template>
+  <!--  Блокируем перенос блоков, если пользователь не вошёл в систему-->
   <div
-    :draggable="true"
+    :draggable="authStore.isAuthenticated"
     @dragstart.self="onDrag"
     @dragover.prevent
     @dragenter.prevent>
     <slot />
   </div>
 </template>
+
 <script setup>
 import { DATA_TRANSFER_PAYLOAD, MOVE } from '../constants';
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
 
 const props = defineProps({
   transferData: {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { clickOutside } from './common/directives';
+import { useAuthStore } from '@/stores';
+import { getToken, removeToken } from '@/services/token-manager';
 
 import App from './App.vue';
 import router from './router';
@@ -9,6 +11,19 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.directive('click-outside', clickOutside);
 
 app.mount('#app');
-app.directive('click-outside', clickOutside);
+
+// Проверяем если пользователь уже вошел в систему
+const token = getToken();
+if (token) {
+  try {
+    const authStore = useAuthStore();
+    await authStore.getMe();
+    await router.push('/');
+  } catch (e) {
+    removeToken();
+    console.log(e);
+  }
+}
